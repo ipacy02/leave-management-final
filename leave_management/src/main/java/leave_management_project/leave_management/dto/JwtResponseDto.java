c0a0f390c19955a7f8a34757ab99a0b35b43bd0a
ipacy02 <63290950+ipacy02@users.noreply.github.com>
2026-02-04 17:24:28 +0200

    Update JwtResponseDto.java

--- leave_management/src/main/java/leave_management_project/leave_management/dto/JwtResponseDto.java
+++ leave_management/src/main/java/leave_management_project/leave_management/dto/JwtResponseDto.java
@@ -1,9 +1,9 @@
 package leave_management_project.leave_management.dto;
 
-import lombok.Data;
+import lombok.Data;   //this is the file I can use 
 
 @Data
-public class JwtResponseDto {
+public class JwtResponseDto { 
     private String token;
 
     public JwtResponseDto(String token) {
