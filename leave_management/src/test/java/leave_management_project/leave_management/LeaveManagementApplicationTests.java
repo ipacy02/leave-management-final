6545f0ceecbad882dec66d9926ca87b338d7f4ad
ipacy02 <63290950+ipacy02@users.noreply.github.com>
2026-02-04 17:15:54 +0200

    Update LeaveManagementApplicationTests.java

--- leave_management/src/test/java/leave_management_project/leave_management/LeaveManagementApplicationTests.java
+++ leave_management/src/test/java/leave_management_project/leave_management/LeaveManagementApplicationTests.java
@@ -1,7 +1,7 @@
 package leave_management_project.leave_management;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;  //use this project
 
 @SpringBootTest
 class LeaveManagementApplicationTests {
